ffe98e206eb5f8e04ce13cd9ba0e5d6914bfa397
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-09 15:36:50 -0800

    3 routes added, mean, median, mode

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>')
-})
+app.get('/mean', (req, res) => {
+    res.send('<h1>mean route</h1>')
+});
+
+app.get('/median', (req, res) => {
+    res.send('<h1>median route')
+});
+
+app.get('/mode', (req, res) => {
+    res.send('<h1>median route')
+});
 
 app.listen(port, () => {
     console.log(`server active at port ${port}`);
