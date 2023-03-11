5732d52cf95dda2b9cebcfd281ff266f02f18342
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-10 20:08:03 -0800

    added isQueryValid that just checks if the query string url has nums as a key, added convertToNumsArray that returns a new array converting from the query string to real int, added getMean that get the average of nums in an array

--- utils.js
+++ utils.js
@@ -1,2 +1,20 @@
-const isQueryValid = (req) => ((req.query.hasOwnProperty('num') && req.query['num'].length > 0)) ? true : false;
-module.exports = isQueryValid;
+const ExpressError = require('./expressError');
+
+const isQueryValid = query => ((query.hasOwnProperty('nums') && query['nums'].length > 0)) ? true : false;
+
+// split query string and convert to int
+const convertToNumsArray = nums => {
+    let numsArr = nums.split(',');
+    return numsArr.map( str => { 
+            if( isNaN(+str) ) throw new ExpressError(`${str} is not a number`, 400);
+            return +str;
+            });
+    };
+
+const getMean = num => num.reduce( (curr, nextVal) => curr + nextVal, 0) / num.length;
+
+module.exports = {
+    isQueryValid,
+    convertToNumsArray,
+    getMean
+}
