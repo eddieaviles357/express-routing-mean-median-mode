20b098500a949a6e2be797a71a27631ff0a2282e
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-13 19:08:09 -0700

    updated and optimized util functions

--- utils.js
+++ utils.js
@@ -1,29 +1,31 @@
 const ExpressError = require('./expressError');
 
-const isQueryValid = query => ((query.hasOwnProperty('nums') && query['nums'].length > 0)) ? true : false;
+const isQueryValid = (query) => (query.hasOwnProperty('nums') && query['nums'].length > 0) ? true : false;
 
 // split query string and convert to int
-const convertToNumsArray = nums => {
+const convertToNumsArray = (nums) => {
     let numsArr = nums.split(',');
-    return numsArr.map( str => { 
-            if( isNaN(+str) ) throw new ExpressError(`${str} is not a number`, 400);
-            return +str;
+    return numsArr.map( (str, idx, arr) => { 
+            if( isNaN(Number(str)) ) throw new ExpressError(`${arr[idx]} is not a number`, 400);
+            return Number(str);
             });
     };
 
 const queryToNumsArr = (query, nums) => {
     if( !isQueryValid(query) ) throw new ExpressError('nums are required', 400);
-    return convertToNumsArray(nums)
+    return convertToNumsArray(nums);
 }
 
-const getMean = arr => arr.reduce( (curr, nextVal) => curr + nextVal, 0) / arr.length;
-const getMedian = arr => {
+const getMean = (arr) => arr.reduce((curr, nextVal) => curr + nextVal)/arr.length;
+
+const getMedian = (arr) => {
     let sortedArr = arr.sort((a,b) => a-b);
     return (arr.length % 2 === 1) ? 
                 sortedArr[ Math.floor( sortedArr.length / 2 ) ] :
                 ( sortedArr[ (sortedArr.length/2) ] + sortedArr[ (sortedArr.length/2) - 1 ]) / 2;
 }
-const getMode = arr => {
+
+const getMode = (arr) => {
     let count = 0;
     let mostFreq = 0;
     let freqArr = arr.reduce((curr, next) => {
